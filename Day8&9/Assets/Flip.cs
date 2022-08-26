56399694ea7ef5fbe17cabfa9332273afa279c75
Abdulrahman Salem alMutairi <aalmutairi2008@gmail.com>
2022-08-27 00:45:42 +0300

    done

--- Day8&9/Assets/Flip.cs
+++ Day8&9/Assets/Flip.cs
@@ -7,6 +7,12 @@ public class Flip : MonoBehaviour
 {
     SpriteRenderer sprite;
     bool faceRight = true;
+
+    public GameObject bullet;
+    GameObject bulletClone;
+
+    public Transform leftSpam;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,7 @@ public class Flip : MonoBehaviour
     void Update()
     {
         FlipPlayer();
+        Fire();
     }
 
     void FlipPlayer()
@@ -30,6 +37,21 @@ public class Flip : MonoBehaviour
         {
             sprite.flipX = true;
             faceRight = false;
+        }
+    }
+    void Fire()
+    {
+        if (Input.GetMouseButtonDown(0) && faceRight)
+        {
+            bulletClone = Instantiate(bullet, transform.position, transform.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+            Destroy(bulletClone, 1.5f);
+        }
+        else if (Input.GetMouseButtonDown(0) && !faceRight)
+        {
+            bulletClone = Instantiate(bullet, leftSpam.position, leftSpam.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().velocity = transform.right * -speed;
+            Destroy(bulletClone, 1.5f);
         }
     }
 
